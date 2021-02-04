357bc43c0d12cf1ac9482cd16e26abc7e97cff62
skeep194 <skeep194@knu.ac.kr>
2021-02-05 02:47:08 +0900

    fix bug

--- back/index.js
+++ back/index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import auth from './router/auth.js'
 const app = express()
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use('/auth', auth);
 app.use('/', (req, res) => {
   res.json({'message': 'hello!'});
