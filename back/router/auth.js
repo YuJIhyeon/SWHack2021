05944959055b2a507777e7d5e8d52af51a9fdb58
skeep194 <skeep194@knu.ac.kr>
2021-02-05 02:09:43 +0900

    add login

--- back/router/auth.js
+++ back/router/auth.js
@@ -1,8 +1,16 @@
 import express from 'express';
 import db from '../config/db.js';
+import crypto from 'crypto-promise';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
-const salt = 'swhackerton';
+const salt = 'swhackaton';
+const jwtSecret = 'swhackaton'
+
+async function pwHash(password) {
+  const hash = await crypto.hash('sha256')(salt+password);
+  return hash;
+}
 
 //사용자 회원가입
 router.post('/register', async (req, res) => {
@@ -15,7 +23,7 @@ router.post('/register', async (req, res) => {
       });
       return;
     }
-    await db.query('INSERT INTO user(ID, Password, Interest, age, sex) VALUE(?, ?, ?, ?, ?)', id, password, interest, age, sex);
+    await db.query('INSERT INTO user(ID, Password, Interest, age, sex) VALUE(?, ?, ?, ?, ?)', id, await pwHash(password), interest, age, sex);
     res.status(200).json({
       message: 'success'
     });
@@ -28,9 +36,35 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
   const {id, password} = req.body;
   try {
-
+    let row = db.query('SELECT * FROM user WHERE ID = ?', id);
+    let data = row[0];
+    if(data.length === 0) {
+      res.status(404).json({
+        message: 'user id not found'
+      });
+      return;
+    }
+    if(data[0].Password !== await pwHash(password)) {
+      res.status(409).json({
+        message: 'password wrong!'
+      });
+      return;
+    }
+    const token = jwt.sign({
+        id: data[0].id,
+      },
+      jwtSecret,
+      {
+        expiresIn: '1d',
+        issuer: 'user'
+      }
+    );
+    res.status(200).json({
+      message: 'success',
+      token: token
+    });
   } catch (error) {
-
+    res.status(500).json(error);
   }
 })
 
