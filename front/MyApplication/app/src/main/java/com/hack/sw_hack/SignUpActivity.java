04799cdb501eb77ffbc47dcb697959d1f466b684
brightdragom <brightdragom@gmail.com>
2021-02-05 09:27:37 +0900

    Splash Activity

--- front/MyApplication/app/src/main/java/com/hack/sw_hack/SignUpActivity.java
+++ front/MyApplication/app/src/main/java/com/hack/sw_hack/SignUpActivity.java
@@ -3,12 +3,26 @@ package com.hack.sw_hack;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class SignUpActivity extends AppCompatActivity {
+    private Spinner spinner_year, spinner_month, spinner_day;
+    private Button backButton, signUpButton;
+    private EditText idEditText, pwEditText, secpwEditText, nameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ArrayList<String> y_list = new ArrayList<>();
+        ArrayList<String> m_list = new ArrayList<>();
+        ArrayList<String> d_list = new ArrayList<>();
+        spinner_year = (Spinner) findViewById(R.id.year);
+        spinner_month = (Spinner) findViewById(R.id.month);
+        spinner_day = (Spinner) findViewById(R.id.day);
     }
 }
