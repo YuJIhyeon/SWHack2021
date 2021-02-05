7c7403a9430c6aff4ae68010f6a033e600ba228a
brightdragom <brightdragom@gmail.com>
2021-02-05 19:33:43 +0900

    write wish

---
+++ front/MyApplication/app/src/main/java/com/hack/sw_hack/popupEditActivity.java
@@ -0,0 +1,14 @@
+package com.hack.sw_hack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class popupEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popup_edit);
+    }
+}
