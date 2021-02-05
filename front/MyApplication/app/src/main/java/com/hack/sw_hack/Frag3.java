b8f59f35629a91769f75595074f85044ecdb6b22
hanamuk <46813878+hanamuk@users.noreply.github.com>
2021-02-06 03:10:49 +0900

    add_Frag3

--- front/MyApplication/app/src/main/java/com/hack/sw_hack/Frag3.java
+++ front/MyApplication/app/src/main/java/com/hack/sw_hack/Frag3.java
@@ -1,9 +1,15 @@
 package com.hack.sw_hack;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,11 +18,42 @@ import androidx.fragment.app.Fragment;
 public class Frag3 extends Fragment {
 
     private View view;
-
+    ImageButton btn;
+    ImageView iv;
+    String[] items = {"글귀", "태그", "분야"};
+    Spinner spinner;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag3, container, false);
+        iv = (ImageView) view.findViewById(R.id.view1);
+        btn = (ImageButton) view.findViewById(R.id.button);
+        spinner = (Spinner) view.findViewById(R.id.since_spinner);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //iv.setVisibility(View.VISIBLE);
+                iv.setImageResource(R.drawable.result);
+            }
+        });
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),R.layout.support_simple_spinner_dropdown_item,items );
+
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        // 스피너에 어댑터 설정
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         return view;
     }
