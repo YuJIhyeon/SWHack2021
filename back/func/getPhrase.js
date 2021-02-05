abf9959ebca32cf417bc884963214a729bc0eb56
skeep194 <skeep194@knu.ac.kr>
2021-02-05 16:47:37 +0900

    fix getPhrase

--- back/func/getPhrase.js
+++ back/func/getPhrase.js
@@ -3,12 +3,12 @@ import db from '../config/db.js'
 export default async (id) => {
   const data = await db.query('SELECT * FROM phrase WHERE id = ?', [id]);
   const attribute = await db.query('SELECT * FROM ' + data[0].referenceName + ' WHERE phraseID = ?', [id]);
-  if(categoryName === 'great') {
+  if(data[0].categoryName === 'great') {
     data[0].name = attribute[0].name;
-  } else if (categoryName === 'media') {
+  } else if (data[0].categoryName === 'media') {
     data[0].title = attribute[0].title;
     data[0].type = attribute[0].type;
-  } else if(categoryName === 'proverb') {
+  } else if(data[0].categoryName === 'proverb') {
     data[0].nation = attribute[0].nation;
   }
   return data[0];
