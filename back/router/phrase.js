3516ee07d88e4d05c28a95ecd10392084042b975
hanamuk <46813878+hanamuk@users.noreply.github.com>
2021-02-05 12:46:39 +0900

    write phrase

--- back/router/phrase.js
+++ back/router/phrase.js
@@ -1,8 +1,27 @@
 import express from 'express'
 
 const router = express.Router()
+
 //명언 작성
-router.post('/');
+router.post('/',  async (req, res) => {
+    const {phrase, categoryName, writerId, referenceName} = req.body;
+    try {
+      const exist = await db.query('SELECT EXISTS(SELECT * FROM phrase WHERE phrase = ?) as isExist', [phrase]);
+      if(exist[0]['isExist']) {
+        res.status(409).json({
+          message: 'exist phrase'
+        });
+        return;
+      }
+      await db.query('INSERT INTO phrase(phrase, categoryName, writerId, referenceName) VALUE(?, ?, ?, ?, ?)', [phrase, categoryName, writerId, referenceName]);
+      res.status(200).json({
+        message: 'success'
+      });
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  })
+  
 
 //명언 불러오기
 router.get('/:id');
